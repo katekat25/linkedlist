321afdc9a10621dd78c92f9339c8c29416dacc41
Kate Schumacher <kate.schumacher@gmail.com>
2025-03-05 14:32:27 -0600

    Add tests

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-import { LinkedList } from "linkedlist.js";
+import { LinkedList } from "./linkedlist.js";
 
 const list = new LinkedList();
 
-list.append("happy");
-list.append("funny");
-list.append("lucky");
-list.toString();
-console.log("Size:", list.getSize()); // Expected: 3
-console.log("Tail:", list.getTail().value); // Expected: "lucky"
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
 
-list.pop();
-console.log("Size after pop:", list.getSize()); // Expected: 2
-console.log("Tail after pop:", list.getTail().value); // Expected: "funny"
-list.toString();
+console.log(list.toString());
